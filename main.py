30828d3c89066d829def359ddb83a78f916bdcb8
bakeryproducts <gleb.m.sokolov>
2017-02-20 18:55:46 +0000

    multi spider

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import random
 import string
-import time
 from urllib.parse import urlparse
 from multiprocessing import Pool
-from urllib.request import urlopen
 
 import bs4 as bs
 import requests
@@ -12,15 +10,18 @@ from urllib.parse import urljoin
 
 def get_links(url):
     try:
-        start_time = time.time()
         print('trying ',url)
-        resp = requests.get(url)
-        print('response!')
+        resp = requests.get(url,timeout=3)
+
         soup = bs.BeautifulSoup(resp.text, 'lxml')
         body = soup.body
         links = [urljoin(url, link.get('href')) for link in body.find_all('a') if is_good(link.get('href'))]
         links.append(url)
         return list(set(links))
+
+    except requests.ReadTimeout:
+        print('cant get response!')
+        return [url]
     except Exception as e:
         #print(e)
         return [url]
@@ -37,41 +38,48 @@ def is_good(url):
 
 def url_gen():
     st = ''.join(random.SystemRandom().choice(string.ascii_lowercase) for _ in range(3))
-    st = ''.join(['http://', st, '.ru'])
+    st = ''.join(['http://', st, '.com'])
     return st
 
 
 def get_dom(url):
-    dom = urlparse(url).hostname
-    dom = ''.join(['http://',dom])
+    try:
+        dom = urlparse(url).hostname
+        dom = ''.join(['http://',dom])
+    except:
+        return ''
     return dom
 
 def main():
     visited_domains = []
     visited_urls = []
-    cnt = 5
+    cnt = 3
     p = Pool(processes=10)
-    #to_parse = [url_gen() for _ in range(cnt)]
-    to_parse = ['http://ed.gov.ru']
+    to_parse = [url_gen() for _ in range(cnt)]
+
+    fd = open('domains.txt','w')
+    fu = open('urls.txt','w')
     i=0
-    while i<10 and len(to_parse)>0 and 'http://www.youtube.com' not in visited_domains:
+    while i<20 and len(to_parse)>0 and 'youtube' not in visited_domains:
         data = p.map(get_links, to_parse)
         links = [url for site in data for url in site]
         print(links)
 
         to_parse = list(set([get_dom(url) for url in links if get_dom(url) not in visited_domains]))
-        print('and parse: ',to_parse)
         visited_urls.extend(links)
         visited_urls = list(set(visited_urls))
         visited_domains.extend([get_dom(link) for link in links])
         visited_domains = list(set(visited_domains))
         i+=1
-
-    with open('domains.txt', 'w') as fd:
         fd.write('\n'.join(visited_domains))
-    with open('urls.txt', 'w') as fu:
         fu.write('\n'.join(visited_urls))
+        print('-------------------------------------end-iteration------------------------------------------------')
+
     p.close()
-    print(len(visited_domains),' ',len(visited_urls))
+    fd.close()
+    fu.close()
+
+    print('THE END: visited {} urls in {} domains. worked for {} iterations'.format(len(visited_domains),len(visited_urls),i))
+
 if __name__ == '__main__':
     main()
